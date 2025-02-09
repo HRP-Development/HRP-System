2eaacf727f22570bd1e1094c284cef882aac81b3
Serpensin <wissmann.matti@gmail.com>
2025-02-09 21:37:54 +0100

    Protokollierung und ID-Listen aktualisiert
    Eine Protokollierungszeile wurde hinzugefügt, um anzuzeigen, dass das Modul eingerichtet wurde.
    Die Liste der erlaubten IDs für `_allowed_cwrp` und `_allowed_jvs` wurde jeweils um einen neuen Eintrag erweitert.

--- gf-system/CustomModules/server_updater.py
+++ gf-system/CustomModules/server_updater.py
@@ -28,7 +28,6 @@ def setup(client:discord.Client, tree: discord.app_commands.CommandTree, server_
 
 
 _allowed_cwrp = [970119359840284743, # Serpensin
-                 556903657250095124, # Justin
                 ]
 _allowed_jvs = [970119359840284743,  # Serpensin
                 587018112134807567,  # Gravefist
